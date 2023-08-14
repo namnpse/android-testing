2ae34073ae49276e48c97d3d71851f48f991d25a
Namnpse <nam444479@gmail.com>
2023-08-14 23:21:29 +0700

    [Shopping] add fragment testing

---
+++ app/src/debug/java/com/namnp/testingandroid/HiltTestActivity.kt
@@ -0,0 +1,7 @@
+package com.namnp.testingandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HiltTestActivity : AppCompatActivity()
