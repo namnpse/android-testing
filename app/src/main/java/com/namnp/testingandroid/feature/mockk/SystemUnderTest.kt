600246b06cf7849fac05e7a13911655189f246cb
Namnpse <nam444479@gmail.com>
2024-08-04 12:52:19 +0900

    [Testing] verify behavior with 4 type: verify, verifyAll, verifySequence, verifyOrder

--- app/src/main/java/com/namnp/testingandroid/feature/mockk/SystemUnderTest.kt
+++ app/src/main/java/com/namnp/testingandroid/feature/mockk/SystemUnderTest.kt
@@ -6,13 +6,33 @@ import kotlin.math.abs
 class Dependency1(val value1: Int) {
     fun calculateAbs(number: Int) = abs(number)
     fun callReturningUnit(number: Int) {}
+    fun functionNeverCalled(number: Int) {}
 }
-class Dependency2(val value2: String)
+class Dependency2(val value2: String) {
+    fun doWork1() {}
+    fun doWork2() {}
+    fun doWork3() {}
+}
 
 // System Under Test (SUT)
 class SystemUnderTest(
     val dependency1: Dependency1,
     val dependency2: Dependency2
 ) {
+
+    /* init {
+        callOneFunctionInDependency1()
+    } */
+
     fun calculate() = dependency1.value1 + (dependency2.value2.toIntOrNull() ?: 0)
+
+    fun callOneFunctionInDependency1() {
+        dependency1.calculateAbs(5)
+    }
+
+    fun doWork() {
+        dependency2.doWork1()
+        dependency2.doWork2()
+        dependency2.doWork3()
+    }
 }
--- app/src/test/java/com/namnp/testingandroid/mockk/SystemUnderTestMockk.kt
+++ app/src/test/java/com/namnp/testingandroid/mockk/SystemUnderTestMockk.kt
@@ -10,6 +10,9 @@ import io.mockk.every
 import io.mockk.just
 import io.mockk.mockk
 import io.mockk.verify
+import io.mockk.verifyAll
+import io.mockk.verifyOrder
+import io.mockk.verifySequence
 import junit.framework.TestCase.assertEquals
 import org.junit.Test
 import org.mockito.Mockito
@@ -98,6 +101,76 @@ class SystemUnderTestMockk {
         // arg<Int>(0)/firstArg() stands for the value of the first argument (index: 0,1,2,...)
         every { doc.calculateAbs(10) } answers { arg<Int>(0) }
         every { doc.calculateAbs(10) } answers { firstArg() }
+    }
+
+    // 3. Behavior verification (verify function call)
+    // 4 types: unordered, ordered, sequential and all
+    @Test
+    fun calculateSumAndVerifyFunctionsCall() {
+        val doc1 = mockk<Dependency1>()
+        val doc2 = mockk<Dependency2>()
+        val doc3 = mockk<Dependency2>()
+
+        every { doc1.value1 } returns 5 // in "every" block, only 1 call is allowed.
+        every { doc2.value2 } returns "6"
+        every { doc1.calculateAbs(any()) }  returns 1
+
+        val sut = SystemUnderTest(doc1, doc2)
+        sut.callOneFunctionInDependency1()
+
+        assertEquals(sut.calculate(), 11)
+        assertThat(sut.calculate()).isEqualTo(11)
+        // Unlike in "every", in "verify" block -> can have several calls going one after another.
+        // 1. Unordered
+        verify {
+            doc1.value1
+            doc2.value2
+            doc1.calculateAbs(5) // check if calculateAbs(5) is called at least once.
+        }
+        // By default, atLeast is 1 and atMost is Int.MAX_VALUE
+        verify(atLeast = 1, atMost = 10) {
+            doc1.calculateAbs(5)
+        }
+        verify(exactly = 2) {// call 2 times since sut.calculate() 2 times assert
+            doc1.value1
+        }
+
+        verify(exactly = 0) {
+            doc1.functionNeverCalled(any())
+        }
+        // used for mock object call, not a function call
+        verify { doc3 wasNot called }
+
+        /* 2. All
+        verifyAll same as verify
+        except it additionally checks that all matched calls are the only calls happened to mentioned mocks.
+        similar to `verifyNoMoreInteractions` in Mockito.
+        i.e: only calculateAbs and value1 are called, no other functions are called in doc1 anymore.
+        */
+        verifyAll {
+            doc1.calculateAbs(5)
+            doc1.value1
+        }
+
+        every { doc2.doWork1() } just Runs
+        every { doc2.doWork2() } just Runs
+        every { doc2.doWork3() } just Runs
+
+        sut.doWork() // Work 1 -> 2 -> 3
+        // verify that the exact sequence happened -> strict order
+        verifySequence {
+            doc2.value2
+            doc2.value2
+            doc2.doWork1()
+            doc2.doWork2()
+            doc2.doWork3()
+        }
+        // verifies only the order of calls that happened,
+        // allowing gaps in the sequence of calls. (missing doWork2() is ok)
+        verifyOrder {
+            doc2.doWork1()
+            doc2.doWork3()
+        }
     }
 
 }
